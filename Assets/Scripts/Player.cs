902bbde78dbb8ce061c49787a89370c399e104c9
Elias <eliasbrucart@hardgames.com.ar>
2021-05-16 02:10:13 -0300

    Added Player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 initialPos;
+    void Start()
+    {
+        transform.position = initialPos;
+    }
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
