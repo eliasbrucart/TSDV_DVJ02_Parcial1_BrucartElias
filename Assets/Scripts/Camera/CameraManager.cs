39ea3af2b61aa4f0fef23da1606937da6b3b301d
Elias <eliasbrucart@hardgames.com.ar>
2021-05-18 17:00:19 -0300

    Added smooth camera

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] Player player;
+    [SerializeField] float cameraSpeed;
+    [SerializeField] Vector3 offset;
+    private void Start()
+    {
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
+    }
+
+    void LateUpdate()
+    {
+        float distance = Vector3.Distance(player.transform.position + offset, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position + offset, (cameraSpeed * distance) * Time.deltaTime);
+    }
+}
