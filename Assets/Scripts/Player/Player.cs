f7d08be2c632a61b67c09981503a6cb797c10f9a
Elias <eliasbrucart@hardgames.com.ar>
2021-05-17 22:19:42 -0300

    New player movement

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 initialPos;
+    [SerializeField] Transform point;
+
+    private float distance;
+    private float minDistance = 0.1f;
+
+    void Start()
+    {
+        transform.position = initialPos;
+    }
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        distance = Vector3.Distance(transform.position, point.position);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && distance < minDistance)
+        {
+            point.position += Vector3.forward;
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow) && distance < minDistance)
+        {
+            point.position += Vector3.back;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && distance < minDistance)
+        {
+            point.position += Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && distance < minDistance)
+        {
+            point.position += Vector3.right;
+        }
+
+        CheckExactMovement();
+    }
+
+    private void CheckExactMovement()
+    {
+        if(transform.position != point.position)
+        {
+            transform.position = Vector3.Lerp(transform.position, point.position, Time.deltaTime * speed);
+        }
+    }
+}
