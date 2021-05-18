25517820b165e9d5b7a6363d40a837e09f85d116
Elias <eliasbrucart@hardgames.com.ar>
2021-05-17 22:48:45 -0300

    Now the lerp works fine in player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,7 +4,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Vector3 initialPos;
-    [SerializeField] Transform point;
+    [SerializeField] Vector3 point;
 
     private float distance;
     private float minDistance = 0.1f;
@@ -12,6 +12,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         transform.position = initialPos;
+        point = transform.position;
     }
     void Update()
     {
@@ -20,22 +21,22 @@ public class Player : MonoBehaviour
 
     private void MovePlayer()
     {
-        distance = Vector3.Distance(transform.position, point.position);
+        distance = Vector3.Distance(transform.position, point);
         if (Input.GetKeyDown(KeyCode.UpArrow) && distance < minDistance)
         {
-            point.position += Vector3.forward;
+            point += Vector3.forward;
         }
         if(Input.GetKeyDown(KeyCode.DownArrow) && distance < minDistance)
         {
-            point.position += Vector3.back;
+            point += Vector3.back;
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow) && distance < minDistance)
         {
-            point.position += Vector3.left;
+            point += Vector3.left;
         }
         if (Input.GetKeyDown(KeyCode.RightArrow) && distance < minDistance)
         {
-            point.position += Vector3.right;
+            point += Vector3.right;
         }
 
         CheckExactMovement();
@@ -43,9 +44,9 @@ public class Player : MonoBehaviour
 
     private void CheckExactMovement()
     {
-        if(transform.position != point.position)
+        if (transform.position != point)
         {
-            transform.position = Vector3.Lerp(transform.position, point.position, Time.deltaTime * speed);
+            transform.position = Vector3.Lerp(transform.position, point, Time.deltaTime * speed);    
         }
     }
 }
