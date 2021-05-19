b32f5ae63c372d025d447927e4a3bf644dbe3c58
Elias <eliasbrucart@hardgames.com.ar>
2021-05-18 21:20:27 -0300

    Now the bomb explodes

--- Assets/Scripts/Player/Bomb/Bomb.cs
+++ Assets/Scripts/Player/Bomb/Bomb.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     [SerializeField] private int damage;
-    [SerializeField] private GameObject bombPrefab;
+    [SerializeField] private float timeToExplode;
+
+    private float timerToExplode;
     private bool destroyed = false;
 
-    private void Start()
-    {
+    static public event Action<Bomb> BombExploded;
 
+    void Start()
+    {
+        timerToExplode = 0.0f;
     }
 
-    private void Update()
+    void Update()
     {
-
+        timerToExplode += Time.deltaTime;
+        if (timerToExplode >= timeToExplode)
+            Explode();
     }
 
     private void Explode()
     {
-
+        if (!destroyed)
+        {
+            Destroy(this.gameObject);
+            timerToExplode = 0.0f;
+            destroyed = true;
+        }
     }
 }
