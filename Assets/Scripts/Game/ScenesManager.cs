9b4218fedddacb442fca5d7acc1e7a5dc35d5f8f
Elias <eliasbrucart@hardgames.com.ar>
2021-05-21 01:40:58 -0300

    Menu buttons working

--- Assets/Scripts/Game/ScenesManager.cs
+++ Assets/Scripts/Game/ScenesManager.cs
@@ -13,7 +13,6 @@ public class ScenesManager : MonoBehaviour
             Destroy(this.gameObject);
         else
             instanceScenesManager = this;
-        DontDestroyOnLoad(this.gameObject);
     }
 
     public void ChangeScene(string scene)
